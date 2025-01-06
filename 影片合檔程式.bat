3d830703690c9576fcde73912a0dfa9d0acd16f5
DESKTOP-JJ5N8QB\tony <tony47586947work@gmail.com>
2025-01-06 16:04:35 +0800

    20250106開發前上傳檔案

---
+++ 影片合檔程式.bat
@@ -0,0 +1,28 @@
+﻿@echo off
+setlocal enabledelayedexpansion
+chcp 65001
+
+@echo off
+set ffmpeg_path="%~dp0ffmpeg-6.1.1-full_build\bin\ffmpeg.exe"
+
+echo 即將合併的文件：
+(for /f "tokens=*" %%a in ('dir /b /on *.m2ts ^| findstr /v /i /c:output.m2ts') do (
+     echo %%a
+))
+
+set /p choice=是否繼續執行？ (Y/N):
+if /i "%choice%"=="N" (
+     echo 使用者選擇中斷執行。
+     pause
+     exit
+)
+
+(for /f "tokens=*" %%a in ('dir /b /on *.m2ts ^| findstr /v /i /c:output.m2ts') do (
+     echo file '%%a'
+)) > m2ts_files.txt
+
+%ffmpeg_path% -safe 0 -ss 0 -f concat -i "%~dp0m2ts_files.txt" -fflags +genpts -vf yadif -c:v h264_nvenc -bf 0 -rc:v vbr -g 15 -b:v 8M -maxrate:v 8M -bufsize:v 8M  -c:a ac3 -b:a 256k -ac 2 -ar 48000 output.m2ts
+
+rem del /q m2ts_files.txt
+
+pause
---
+++ 影片轉音檔程式.bat
@@ -0,0 +1,11 @@
+﻿@echo off
+
+chcp 65001
+
+@echo off
+set ffmpeg_path="%~dp0ffmpeg-6.1.1-full_build\bin\ffmpeg.exe"
+
+:softshare
+if "%~1"=="" goto:eof
+%ffmpeg_path% -i "%~1" -c:a pcm_s16le "%~dpn1.wav"
+shift&goto:softshare
